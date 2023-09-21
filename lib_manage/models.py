4f2da7fee7030953d6fc71bc4caafb00b5958cbc
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-21 15:08:21 +0330

    Added BorrowedBook model to models.py

--- lib_manage/models.py
+++ lib_manage/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+
+from datetime import datetime, timedelta
 
 class Person(models.Model):
     name = models.CharField(max_length=300)
@@ -20,4 +23,17 @@ class Book(models.Model):
     
     def __str__(self):
         return f"{self.title} {self.number_of_available}"
+
+
+class BorrowedBook(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    borrowed_date = models.DateTimeField(default=timezone.now)
+    must_return_date = models.DateTimeField(default=(timezone.now() + timedelta(days=10)))
+    actual_return_date = models.DateTimeField(null=True, blank=True)
+    return_book = models.OneToOneField('ReturnBook', on_delete=models.CASCADE, null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.person} borrowed {self.book}"
     
