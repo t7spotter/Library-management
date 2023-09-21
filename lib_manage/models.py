c188594e85da927b13a18aa753875a69d1a28a15
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-21 14:21:58 +0330

    Added Person model to models.py

--- lib_manage/models.py
+++ lib_manage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=300)
+    tell = models.CharField(max_length=11)
+    date = models.DateTimeField(auto_now=True, auto_now_add=False)
+    balance = models.DecimalField(decimal_places=2, max_digits=8, default=0)
+    
+    def __str__(self):
+        return self.name
