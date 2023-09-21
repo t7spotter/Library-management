52780d479a5f3cb33ac3e18578389611983167ba
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-21 15:22:56 +0330

    Added BookSerializers model to serializers.py

--- lib_manage/serializers.py
+++ lib_manage/serializers.py
@@ -7,3 +7,12 @@ class PersonSerializers(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'
+        
+
+class BookSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        depth = 1
+        
+
