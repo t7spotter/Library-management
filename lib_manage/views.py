66ee27f6aa9320465650ca92427688d1ca59fadb
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-21 15:35:03 +0330

    Added get method for GetPostPerson view

--- lib_manage/views.py
+++ lib_manage/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
 
-# Create your views here.
+from django.utils import timezone
+
+from .models import Person, Book, BorrowedBook, ReturnBook
+from .serializers import PersonSerializers, BookSerializers, BorrowedBookSerializers, ReturnBookSerializers, ContainSearchSerializer
+
+
+class GetPostPerson(APIView):
+    def get(self, request: Request, pk=None):
+        if pk:
+            query = Person.objects.get(pk=pk)
+            ser = PersonSerializers(query)
+            return Response(ser.data, status=status.HTTP_200_OK)
+        else:
+            query = Person.objects.all()
+            ser = PersonSerializers(query, many=True)
+            return Response(ser.data, status=status.HTTP_200_OK)
