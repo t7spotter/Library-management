ed9bb307a320732cadd876417ce70821a8b67cb0
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-21 17:10:56 +0330

    Representing models admin on admin.py

--- lib_manage/admin.py
+++ lib_manage/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, Book, BorrowedBook, ReturnBook
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'number_of_available')
+
+class BorrowedBookAdmin(admin.ModelAdmin):
+    list_display = ('person', 'book', 'borrowed_date', 'must_return_date', 'actual_return_date', 'is_returned')
+
+
+####
+class BorrowedBookInline(admin.TabularInline):
+    model = BorrowedBook
+    extra = 0
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [BorrowedBookInline]
+###    
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BorrowedBook, BorrowedBookAdmin)
+admin.site.register(ReturnBook)
