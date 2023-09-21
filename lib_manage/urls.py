ec3685c1bae13b5e36fec8ea3540b6681030caf3
Mohamadali Firoozi <alifiroozi@icloud.com>
2023-09-21 16:15:28 +0330

    Added GetPostBook urls to urls.py

--- lib_manage/urls.py
+++ lib_manage/urls.py
@@ -6,4 +6,7 @@ from lib_manage import views
 urlpatterns = [
     path('get-post-person', views.GetPostPerson.as_view()),
     path('get-post-person/<int:pk>', views.GetPostPerson.as_view()),
+    
+    path('get-post-book', views.GetPostBook.as_view()),
+    path('get-post-book/<int:pk>', views.GetPostBook.as_view()),
 ]
